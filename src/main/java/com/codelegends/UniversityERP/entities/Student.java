10cd48238dac88efa09429a0ae69275df470aabd
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 11:16:18 +0400

    fix: inherit Student entity from BaseClass

--- src/main/java/com/codelegends/UniversityERP/entities/Student.java
+++ src/main/java/com/codelegends/UniversityERP/entities/Student.java
@@ -2,18 +2,27 @@ package com.codelegends.UniversityERP.entities;
 
 import com.codelegends.UniversityERP.enums.Gender;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.OneToMany;
+
 import java.util.ArrayList;
 import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-public class Student {
+public class Student extends BaseClass {
+
     @Column(length = 100, nullable = false)
     private String name;
 
