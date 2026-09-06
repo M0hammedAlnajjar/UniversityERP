e788236c01d6a1300fdda479394bfae1d62e0e31
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 11:17:29 +0400

    fix: remove invalid Guardian self relationship

--- src/main/java/com/codelegends/UniversityERP/entities/Guardian.java
+++ src/main/java/com/codelegends/UniversityERP/entities/Guardian.java
@@ -1,19 +1,21 @@
 package com.codelegends.UniversityERP.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @Entity
-public class Guardian extends BaseClass{
+public class Guardian extends BaseClass {
+
     @Column(length = 100, nullable = false)
     private String name;
 
@@ -22,9 +24,6 @@ public class Guardian extends BaseClass{
 
     @Column(length = 20, nullable = false)
     private String phoneNumber;
-
-    @OneToMany(mappedBy = "student")
-    private List<Guardian> guardians = new ArrayList<>();
 
     @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
