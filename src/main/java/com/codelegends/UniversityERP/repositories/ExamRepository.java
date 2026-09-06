20f75764439be52f8b13cd2b71111b9f6c02a1f2
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 15:37:55 +0400

    feat: add exam course query

--- src/main/java/com/codelegends/UniversityERP/repositories/ExamRepository.java
+++ src/main/java/com/codelegends/UniversityERP/repositories/ExamRepository.java
@@ -2,14 +2,18 @@ package com.codelegends.UniversityERP.repositories;
 
 import com.codelegends.UniversityERP.entities.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-
-public interface ExamRepository  extends JpaRepository<Exam,Long> {
+public interface ExamRepository extends JpaRepository<Exam, Long> {
     List<Exam> findAllByIsActiveTrue();
     Optional<Exam> findByIdAndIsActiveTrue(Long id);
+
+    @Query("select e from Exam e where e.course.id = :courseId and e.isActive = true order by e.examDate")
+    List<Exam> findActiveExamsByCourseId(@Param("courseId") Long courseId);
 }
