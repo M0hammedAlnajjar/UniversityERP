b8fb83fc78b3a1ebf451565b6876f8051af87cbb
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 15:33:44 +0400

    feat: add ResourceNotFoundException

---
+++ src/main/java/com/codelegends/UniversityERP/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.codelegends.UniversityERP.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
