e08a68b2cc23c720fc32ad3c4e65b790956b25e3
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 15:43:11 +0400

    feat: add metric response DTO

---
+++ src/main/java/com/codelegends/UniversityERP/dto/MetricDTO.java
@@ -0,0 +1,16 @@
+package com.codelegends.UniversityERP.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MetricDTO {
+    private Long entityId;
+    private String metric;
+    private Double value;
+}
