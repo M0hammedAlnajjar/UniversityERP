523570bae61c44bd62533c6bfda64b4b7a3a5321
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 15:38:47 +0400

    feat: add classroom capacity query

--- src/main/java/com/codelegends/UniversityERP/repositories/ClassroomRepository.java
+++ src/main/java/com/codelegends/UniversityERP/repositories/ClassroomRepository.java
@@ -2,13 +2,18 @@ package com.codelegends.UniversityERP.repositories;
 
 import com.codelegends.UniversityERP.entities.Classroom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ClassroomRepository extends JpaRepository<Classroom,Long> {
+public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
     List<Classroom> findAllByIsActiveTrue();
     Optional<Classroom> findByIdAndIsActiveTrue(Long id);
+
+    @Query("select max(c.capacity) from Classroom c where c.department.id = :departmentId and c.isActive = true")
+    Integer findMaxActiveCapacityByDepartmentId(@Param("departmentId") Long departmentId);
 }
