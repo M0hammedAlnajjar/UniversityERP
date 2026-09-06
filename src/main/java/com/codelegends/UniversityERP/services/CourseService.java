45ae9f5229d31f40a97799cc9da3a9f75700bf88
mohammed <mooodo150@gmail.com>
2026-09-06 14:19:57 +0400

    feat: add Course create operation

--- src/main/java/com/codelegends/UniversityERP/services/CourseService.java
+++ src/main/java/com/codelegends/UniversityERP/services/CourseService.java
@@ -3,7 +3,11 @@ package com.codelegends.UniversityERP.services;
 
 import com.codelegends.UniversityERP.repositories.CourseRepository;
 import org.springframework.stereotype.Service;
+import com.codelegends.UniversityERP.entities.Course;
+import com.codelegends.UniversityERP.entities.Instructor;
+import com.codelegends.UniversityERP.entities.Program;
 
+import java.util.Date;
 @Service
 public class CourseService {
     private final CourseRepository courseRepository;
@@ -18,6 +22,67 @@ public class CourseService {
         this.courseRepository = courseRepository;
         this.programService = programService;
         this.instructorService = instructorService;
+    }
+
+    public Course createCourse(Course course) {
+
+        if (course == null) {
+            throw new IllegalArgumentException(
+                    "Course cannot be null"
+            );
+        }
+
+        if (courseRepository.existsByCourseCode(
+                course.getCourseCode()
+        )) {
+            throw new IllegalArgumentException(
+                    "Course code already exists"
+            );
+        }
+
+        if (course.getProgram() == null
+                || course.getProgram().getId() <= 0) {
+
+            throw new IllegalArgumentException(
+                    "Program ID is required"
+            );
+        }
+
+        Program program = programService
+                .getProgramById(course.getProgram().getId())
+                .orElseThrow(
+                        () -> new IllegalArgumentException(
+                                "Active program not found"
+                        )
+                );
+
+        course.setProgram(program);
+
+        if (course.getInstructor() != null) {
+
+            if (course.getInstructor().getId() <= 0) {
+                throw new IllegalArgumentException(
+                        "Valid instructor ID is required"
+                );
+            }
+
+            Instructor instructor = instructorService
+                    .getInstructorById(
+                            course.getInstructor().getId()
+                    )
+                    .orElseThrow(
+                            () -> new IllegalArgumentException(
+                                    "Active instructor not found"
+                            )
+                    );
+
+            course.setInstructor(instructor);
+        }
+
+        course.setActive(true);
+        course.setCreatedDate(new Date());
+
+        return courseRepository.save(course);
     }
 
 }
