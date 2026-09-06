e22341fd6c26a59970d80d76df314506f40d52bb
mohammed <mooodo150@gmail.com>
2026-09-06 09:57:52 +0400

    feat: create Enrollment entity

---
+++ src/main/java/com/codelegends/UniversityERP/entities/Enrollment.java
@@ -0,0 +1,25 @@
+package com.codelegends.UniversityERP.entities;
+
+import com.codelegends.UniversityERP.enums.EnrollmentStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Enrollment {
+    @Column(nullable = false)
+    private LocalDate enrollmentDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, nullable = false)
+    private EnrollmentStatus status;
+}
