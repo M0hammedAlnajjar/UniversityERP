ec3d6b9c738d266e2c57c34d05e660b520f3f711
mohammed <mooodo150@gmail.com>
2026-09-06 14:43:42 +0400

    feat: add get all active enrollments operation

--- src/main/java/com/codelegends/UniversityERP/services/EnrollmentService.java
+++ src/main/java/com/codelegends/UniversityERP/services/EnrollmentService.java
@@ -2,6 +2,8 @@ package com.codelegends.UniversityERP.services;
 
 import com.codelegends.UniversityERP.repositories.EnrollmentRepository;
 import org.springframework.stereotype.Service;
+import com.codelegends.UniversityERP.entities.Enrollment;
+import java.util.List;
 
 @Service
 public class EnrollmentService {
@@ -18,5 +20,10 @@ public class EnrollmentService {
         this.enrollmentRepository = enrollmentRepository;
         this.studentService = studentService;
         this.courseService = courseService;
+    }
+
+    public List<Enrollment> getAllEnrollments() {
+
+        return enrollmentRepository.findAllByIsActiveTrue();
     }
 }
