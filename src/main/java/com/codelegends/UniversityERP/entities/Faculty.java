121c070faceeda3e87b48bd72fec4ab3f31b4105
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 11:16:36 +0400

    fix: correct Faculty and University relationship

--- src/main/java/com/codelegends/UniversityERP/entities/Faculty.java
+++ src/main/java/com/codelegends/UniversityERP/entities/Faculty.java
@@ -1,7 +1,12 @@
 package com.codelegends.UniversityERP.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,13 +19,15 @@ import java.util.List;
 @NoArgsConstructor
 @Entity
 public class Faculty extends BaseClass {
+
     @Column(nullable = false, length = 100)
     private String name;
+
     @Column(length = 255)
     private String description;
 
     @JsonIgnore
-    @OneToMany(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "university_id", nullable = false)
     private University university;
 
