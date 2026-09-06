c151e60611db820e29b94ebb3ec561b67c3c95ed
mohammed <mooodo150@gmail.com>
2026-09-06 11:45:50 +0400

    feat: create Exam repository

---
+++ src/main/java/com/codelegends/UniversityERP/repositories/ExamRepository.java
@@ -0,0 +1,15 @@
+package com.codelegends.UniversityERP.repositories;
+
+import com.codelegends.UniversityERP.entities.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+
+public interface ExamRepository  extends JpaRepository<Exam,Long> {
+    List<Exam> findAllByIsActiveTrue();
+    Optional<Exam> findByIdAndIsActiveTrue(Long id);
+}
