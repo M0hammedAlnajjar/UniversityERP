4f5933eee38e81c6e00fc95fa6f527c8866321b1
mohammed <mooodo150@gmail.com>
2026-09-06 12:03:34 +0400

    feat: add University create operation

--- src/main/java/com/codelegends/UniversityERP/services/UniversityService.java
+++ src/main/java/com/codelegends/UniversityERP/services/UniversityService.java
@@ -2,6 +2,8 @@ package com.codelegends.UniversityERP.services;
 
 import com.codelegends.UniversityERP.repositories.UniversityRepository;
 import org.springframework.stereotype.Service;
+import com.codelegends.UniversityERP.entities.University;
+import java.util.Date;
 
 @Service
 public class UniversityService {
@@ -10,5 +12,19 @@ public class UniversityService {
     public UniversityService(UniversityRepository universityRepository) {
         this.universityRepository = universityRepository;
 
+    }
+
+    public University createUniversity(University university) {
+
+        if (university == null) {
+            throw new IllegalArgumentException(
+                    "University cannot be null"
+            );
+        }
+
+        university.setActive(true);
+        university.setCreatedDate(new Date());
+
+        return universityRepository.save(university);
     }
 }
