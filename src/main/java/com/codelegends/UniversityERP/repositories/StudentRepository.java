02dc61d5785b23971e05d78f246c42911978c048
mohammed <mooodo150@gmail.com>
2026-09-06 11:42:48 +0400

    feat: create Student repository

---
+++ src/main/java/com/codelegends/UniversityERP/repositories/StudentRepository.java
@@ -0,0 +1,16 @@
+package com.codelegends.UniversityERP.repositories;
+
+import com.codelegends.UniversityERP.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+    List<Student> findAllByIsActiveTrue();
+    Optional<Student> findByIdAndIsActiveTrue(Long id);
+}
