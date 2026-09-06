eb39f46d6c6d46ac6ce29975ff7b16c239cedf68
mohammed <mooodo150@gmail.com>
2026-09-06 14:41:59 +0400

    feat: add active enrollment duplication query

--- src/main/java/com/codelegends/UniversityERP/repositories/EnrollmentRepository.java
+++ src/main/java/com/codelegends/UniversityERP/repositories/EnrollmentRepository.java
@@ -4,7 +4,7 @@ import com.codelegends.UniversityERP.entities.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+import com.codelegends.UniversityERP.enums.EnrollmentStatus;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +12,9 @@ import java.util.Optional;
 public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
     List<Enrollment> findAllByIsActiveTrue();
     Optional<Enrollment> findByIdAndIsActiveTrue(Long id);
+    boolean existsByStudent_IdAndCourse_IdAndStatusAndIsActiveTrue(
+            Long studentId,
+            Long courseId,
+            EnrollmentStatus status
+    );
 }
