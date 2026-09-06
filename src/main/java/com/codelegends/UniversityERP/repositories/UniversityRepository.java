6aa3fc2d3849145840d038ca7dd61e4f0250b4b8
mohammed <mooodo150@gmail.com>
2026-09-06 11:28:39 +0400

    feat: create University repository

---
+++ src/main/java/com/codelegends/UniversityERP/repositories/UniversityRepository.java
@@ -0,0 +1,18 @@
+package com.codelegends.UniversityERP.repositories;
+
+import com.codelegends.UniversityERP.entities.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+
+public interface UniversityRepository extends JpaRepository<University, Long> {
+
+    List<University> findAllByIsActiveTrue();
+
+    Optional<University> findByIdAndIsActiveTrue(Long id);
+
+}
