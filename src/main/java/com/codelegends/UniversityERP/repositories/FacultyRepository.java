b11c84afb1dc1cb7ac704234098bfae6b9fbd4fa
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 15:38:30 +0400

    feat: add faculty university stats query

--- src/main/java/com/codelegends/UniversityERP/repositories/FacultyRepository.java
+++ src/main/java/com/codelegends/UniversityERP/repositories/FacultyRepository.java
@@ -2,15 +2,18 @@ package com.codelegends.UniversityERP.repositories;
 
 import com.codelegends.UniversityERP.entities.Faculty;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
     List<Faculty> findAllByIsActiveTrue();
     Optional<Faculty> findByIdAndIsActiveTrue(Long id);
+
+    @Query("select count(f) from Faculty f where f.university.id = :universityId and f.isActive = true")
+    long countActiveFacultiesByUniversityId(@Param("universityId") Long universityId);
 }
