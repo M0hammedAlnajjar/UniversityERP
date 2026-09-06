1b389921523e9b0d5a3207275a85b7fc71756149
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 15:38:41 +0400

    feat: add department university stats query

--- src/main/java/com/codelegends/UniversityERP/repositories/DepartmentRepository.java
+++ src/main/java/com/codelegends/UniversityERP/repositories/DepartmentRepository.java
@@ -2,13 +2,18 @@ package com.codelegends.UniversityERP.repositories;
 
 import com.codelegends.UniversityERP.entities.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface DepartmentRepository extends JpaRepository<Department,Long> {
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
     List<Department> findAllByIsActiveTrue();
     Optional<Department> findByIdAndIsActiveTrue(Long id);
+
+    @Query("select count(d) from Department d where d.faculty.university.id = :universityId and d.isActive = true")
+    long countActiveDepartmentsByUniversityId(@Param("universityId") Long universityId);
 }
