8b78d9ca5614cb31414471d1e74fb8b398d3c3bb
Mohammed Salim <mooodo150@gmail.com>
2026-09-06 15:33:50 +0400

    feat: add ErrorResponse record

---
+++ src/main/java/com/codelegends/UniversityERP/exceptions/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.codelegends.UniversityERP.exceptions;
+
+import java.time.LocalDateTime;
+
+public record ErrorResponse(
+        int status,
+        String error,
+        String message,
+        LocalDateTime timestamp
+) {
+}
