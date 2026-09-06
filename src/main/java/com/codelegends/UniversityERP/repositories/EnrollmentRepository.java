75e696277eb6c888f8c9fef29322dea5b68e8ba4
mohammed <mooodo150@gmail.com>
2026-09-06 11:44:31 +0400

    feat: create Enrollment repository

---
+++ src/main/java/com/codelegends/UniversityERP/repositories/EnrollmentRepository.java
@@ -0,0 +1,15 @@
+package com.codelegends.UniversityERP.repositories;
+
+import com.codelegends.UniversityERP.entities.Enrollment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface EnrollmentRepository extends JpaRepository<Enrollment,Long> {
+    List<Enrollment> findAllByIsActiveTrue();
+    Optional<Enrollment> findByIdAndIsActiveTrue(Long id);
+}
