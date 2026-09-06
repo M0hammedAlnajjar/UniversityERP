e32d8505105d4e81cd67629b3972ae07ae754c96
mohammed <mooodo150@gmail.com>
2026-09-06 10:27:30 +0400

    feat: create Exam entity

---
+++ src/main/java/com/codelegends/UniversityERP/entities/Classroom.java
@@ -0,0 +1,26 @@
+package com.codelegends.UniversityERP.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Classroom extends BaseClass {
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private LocalDate examDate;
+
+    @Column(nullable = false)
+    private Double totalMarks;
+
+}
